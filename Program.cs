617c88d617dfe067117d86d7fea99010fd5fac0f
jedediahfanuel <jedediahf51@gmail.com>
2024-07-11 23:49:51 +0700

    🍞 :: random overloaded

--- Program.cs
+++ Program.cs
@@ -14,3 +14,13 @@ string text = "seven";
 Console.WriteLine(number);
 Console.WriteLine();
 Console.WriteLine(text);
+
+// Overloaded Random
+Random diced = new Random();
+int roll1 = dice.Next();
+int roll2 = dice.Next(101);
+int roll3 = dice.Next(50,101);
+
+Console.WriteLine($"First roll: {roll1}");
+Console.WriteLine($"First roll: {roll2}");
+Console.WriteLine($"First roll: {roll3}");
