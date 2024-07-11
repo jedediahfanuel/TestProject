e8a8ea3ae4b871a713b5771e99ac0f1b6ec75a9c
jedediahfanuel <jedediahf51@gmail.com>
2024-07-11 23:46:01 +0700

    🫙 :: console WL as overloaded example

--- Program.cs
+++ Program.cs
@@ -5,3 +5,12 @@ int roll = dice.Next(1, 7);
 Console.WriteLine(roll);
 
 Console.WriteLine(dice.Next(1,20));
+
+
+// Overloaded Methods
+int number = 7;
+string text = "seven";
+
+Console.WriteLine(number);
+Console.WriteLine();
+Console.WriteLine(text);
