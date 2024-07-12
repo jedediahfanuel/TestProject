23652bb3562d156ae63ff480484fbc6958ea111e
jedediahfanuel <jedediahf51@gmail.com>
2024-07-12 23:31:37 +0700

    🎒 :: renew

--- renewal-rate/Program.cs
+++ renewal-rate/Program.cs
@@ -2,4 +2,26 @@
 int daysUntilExpiration = random.Next(12);
 int discountPercentage = 0;
 
-// Your code goes here
+if (daysUntilExpiration == 0)
+{
+	Console.WriteLine("Your subscription has expired.");
+}
+else if (daysUntilExpiration == 1)
+{
+	Console.WriteLine("Your subscription expires within a day!");
+	discountPercentage += 20;
+}
+else if (daysUntilExpiration <= 5)
+{
+	Console.WriteLine($"Your subscription expires in {daysUntilExpiration } days.");
+	discountPercentage += 10;
+}
+else
+{
+	Console.WriteLine($"Your subscription will expire soon. Renew now!");
+}
+
+if (discountPercentage > 0)
+{
+	Console.WriteLine($"Renew now and save {discountPercentage}!");
+}
