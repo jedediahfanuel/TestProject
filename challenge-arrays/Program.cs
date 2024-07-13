cdb23f9f57987665663459eb0b6941206813008c
jedediahfanuel <jedediahf51@gmail.com>
2024-07-13 14:23:29 +0700

    🪄 :: define fOIDs array

--- challenge-arrays/Program.cs
+++ challenge-arrays/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] fraudOIDs = {
+	"B123",
+    "C234",
+    "A345",
+     "C15",
+    "B177",
+   "G3003",
+    "C235",
+    "B179"
+};
+
+
