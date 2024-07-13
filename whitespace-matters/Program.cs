2d3d15e488af6fb4c95a2d5cbd44df9df890c875
jedediahfanuel <jedediahf51@gmail.com>
2024-07-13 14:47:21 +0700

    🎴 :: example 3 as good example

--- whitespace-matters/Program.cs
+++ whitespace-matters/Program.cs
@@ -10,13 +10,16 @@ WriteLine
 // Example 2:
 string firstWord="Hello";string lastWord="Example 2";Console.WriteLine(firstWord+" "+lastWord+"!");
 
-// Example 3:
+// Example 3: Good
 Random dice = new Random();
+
 int roll1 = dice.Next(1, 7);
 int roll2 = dice.Next(1, 7);
 int roll3 = dice.Next(1, 7);
+
 int total = roll1 + roll2 + roll3;
 Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
 if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3)) {
     if ((roll1 == roll2) && (roll2 == roll3)) {
         Console.WriteLine("You rolled triples!  +6 bonus to total!");
