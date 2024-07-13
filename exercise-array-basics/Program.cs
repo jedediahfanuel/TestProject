91a9b38e0cfb0f9bd52a2df06e6b513939fb8242
jedediahfanuel <jedediahf51@gmail.com>
2024-07-13 14:12:45 +0700

    ♟️  :: another array exercise | int

--- exercise-array-basics/Program.cs
+++ exercise-array-basics/Program.cs
@@ -31,3 +31,25 @@ Console.WriteLine($"Reassign First: {fraudulentOrderIds[0]}");
 Console.WriteLine("");
 
 Console.WriteLine($"There are {fraudulentOrderIDs.Length} fraudulent order to process.");
+
+Console.WriteLine("\n\n\n");
+
+Console.WriteLine("// --------------------------------------------------------------\n");
+// --------------------------------------------------------------
+// --------------------------------------------------------------
+
+string[] names = { "Rowena", "Robin", "Bao" };
+foreach (string name in names)
+{
+	Console.WriteLine(name);
+}
+
+int[] inventory = { 200, 450, 700, 175, 250 };
+int sum = 0;
+
+foreach (int items in inventory)
+{
+	sum += items;
+}
+
+Console.WriteLine($"We have {sum} items in inventory");
