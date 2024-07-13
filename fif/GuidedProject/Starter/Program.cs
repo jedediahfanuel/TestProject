7ebfb165e2c551fdf89c6846ee4924379d20821f
jedediahfanuel <jedediahf51@gmail.com>
2024-07-14 01:17:49 +0700

    🧑 :: extra scores

--- fif/GuidedProject/Starter/Program.cs
+++ fif/GuidedProject/Starter/Program.cs
@@ -1,10 +1,10 @@
 ﻿// initialize variables - graded assignments
-int currentAssignments = 5;
+int examAssignments = 5;
 
-int[] sophiaScores = new int[] { 90, 86, 87, 98, 100 };
-int[] andrewScores = new int[] { 92, 89, 81, 96, 90 };
-int[] emmaScores = new int[] { 90, 85, 87, 98, 68 };
-int[] loganScores = new int[] { 90, 95, 87, 88, 96 };
+int[] sophiaScores = new int[] { 90, 86, 87, 98, 100, 94, 90 };
+int[] andrewScores = new int[] { 92, 89, 81, 96, 90, 89 };
+int[] emmaScores = new int[] { 90, 85, 87, 98, 68, 89, 89, 89 };
+int[] loganScores = new int[] { 90, 95, 87, 88, 96, 96 };
 
 // Student names
 string[] studentNames = new string[] { "Sophia", "Andrew", "Emma", "Logan" };
@@ -13,7 +13,7 @@ int[] studentScores = new int[10];
 
 string currentStudentLetterGrade = "";
 
-// Display the Report Header
+// Write the Report Header to the console
 Console.WriteLine("Student\t\tGrade\n");
 
 foreach (string name in studentNames)
@@ -21,19 +21,15 @@ foreach (string name in studentNames)
     string currentStudent = name;
 
     if (currentStudent == "Sophia")
-        // assign Sophia's scores to the studentScores array 
-        studentScores = sophiaScores;
+       studentScores = sophiaScores;
 
     else if (currentStudent == "Andrew")
-        // assign Andrew's scores to the studentScores array 
         studentScores = andrewScores;
 
     else if (currentStudent == "Emma")
-        // assign Emma's scores to the studentScores array 
         studentScores = emmaScores;
 
     else if (currentStudent == "Logan")
-        // assign Logan's scores to the studentScores array 
         studentScores = loganScores;
 
     // initialize/reset the sum of scored assignments
@@ -42,13 +38,25 @@ foreach (string name in studentNames)
     // initialize/reset the calculated average of exam + extra credit scores
     decimal currentStudentGrade = 0;
 
+    // initialize/reset a counter for the number of assignment 
+    int gradedAssignments = 0;
+
+    // loop through the scores array and complete calculations for currentStudent
     foreach (int score in studentScores)
     {
-        // add the exam score to the sum
-        sumAssignmentScores += score;
+        // increment the assignment counter
+        gradedAssignments += 1;
+
+        if (gradedAssignments <= examAssignments)
+            // add the exam score to the sum
+            sumAssignmentScores += score;
+
+        else
+            // add the extra credit points to the sum - bonus points equal to 10% of an exam score
+            sumAssignmentScores += score / 10;
     }
 
-    currentStudentGrade = (decimal)(sumAssignmentScores) / currentAssignments;
+    currentStudentGrade = (decimal)(sumAssignmentScores) / examAssignments;
 
     if (currentStudentGrade >= 97)
         currentStudentLetterGrade = "A+";
@@ -89,8 +97,10 @@ foreach (string name in studentNames)
     else
         currentStudentLetterGrade = "F";
 
-    Console.WriteLine($"{name}\t\t{currentStudentGrade}\t?");
+    //Console.WriteLine("Student\t\tGrade\tLetter Grade\n");
+    Console.WriteLine($"{currentStudent}\t\t{currentStudentGrade}\t{currentStudentLetterGrade}");
 }
 
-Console.WriteLine("Press the Enter key to continue");
+// required for running in VS Code (keeps the Output windows open to view results)
+Console.WriteLine("\n\rPress the Enter key to continue");
 Console.ReadLine();
