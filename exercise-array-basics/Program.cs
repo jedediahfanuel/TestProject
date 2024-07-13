d29fd6db798ddec61a0d827b5dc579430feb916f
jedediahfanuel <jedediahf51@gmail.com>
2024-07-13 13:55:35 +0700

    🐚 :: initializing array

--- exercise-array-basics/Program.cs
+++ exercise-array-basics/Program.cs
@@ -9,3 +9,22 @@ Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
 Console.WriteLine($"Sencond: {fraudulentOrderIDs[1]}");
 Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
 
+fraudulentOrderIDs[0] = "F000";
+
+Console.WriteLine($"Reassign First: {fraudulentOrderIDs[0]}");
+
+Console.WriteLine("");
+Console.WriteLine("");
+Console.WriteLine("");
+
+// Initializing array
+string[] fraudulentOrderIds = { "A123", "B456", "C789" };
+
+Console.WriteLine($"First: {fraudulentOrderIds[0]}");
+Console.WriteLine($"Sencond: {fraudulentOrderIds[1]}");
+Console.WriteLine($"Third: {fraudulentOrderIds[2]}");
+
+fraudulentOrderIds[0] = "F000";
+
+Console.WriteLine($"Reassign First: {fraudulentOrderIds[0]}");
+
