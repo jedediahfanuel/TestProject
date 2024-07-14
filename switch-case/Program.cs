d12e8c2ea2c69eb687b3240ca879f61538ec1c89
jedediahfanuel <jedediahf51@gmail.com>
2024-07-14 17:09:25 +0700

    🍯 :: initial switch case

---
+++ switch-case/Program.cs
@@ -0,0 +1,14 @@
+﻿switch (fruit)
+{
+    case "apple":
+        Console.WriteLine($"App will display information for apple.");
+        break;
+
+    case "banana":
+        Console.WriteLine($"App will display information for banana.");
+        break;
+
+    case "cherry":
+        Console.WriteLine($"App will display information for cherry.");
+        break;
+}
