8bcea2060e11b2aa8edc7754e3fce5f0c8ccc450
jedediahfanuel <jedediahf51@gmail.com>
2024-07-14 19:30:20 +0700

    🌧️  :: while, continue

--- do-while/Program.cs
+++ do-while/Program.cs
@@ -1,4 +1,5 @@
-﻿Random random = new Random();
+﻿// Do While
+Random random = new Random();
 int current = 0;
 
 do
@@ -6,3 +7,27 @@ do
     current = random.Next(1, 11);
     Console.WriteLine(current);
 } while (current != 7);
+
+// While
+Random randoms = new Random();
+int curr = randoms.Next(1, 11);
+
+while (curr >= 3)
+{
+    Console.WriteLine(curr);
+    curr = randoms.Next(1, 11);
+}
+Console.WriteLine($"Last number: {curr}");
+
+// Continue keyword
+Random rand = new Random();
+int cu = rand.Next(1, 11);
+
+do
+{
+    cu = rand.Next(1, 11);
+
+    if (cu >= 8) continue;
+
+    Console.WriteLine(cu);
+} while (cu != 7);
