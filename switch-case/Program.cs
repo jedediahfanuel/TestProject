dff5f17e8836928efac94ace6c17bfe40bf46f92
jedediahfanuel <jedediahf51@gmail.com>
2024-07-14 17:11:28 +0700

    🎽 :: second example

--- switch-case/Program.cs
+++ switch-case/Program.cs
@@ -1,14 +1,25 @@
-﻿switch (fruit)
+﻿int employeeLevel = 200;
+string employeeName = "John Smith";
+
+string title = "";
+
+switch (employeeLevel)
 {
-    case "apple":
-        Console.WriteLine($"App will display information for apple.");
+    case 100:
+        title = "Junior Associate";
         break;
-
-    case "banana":
-        Console.WriteLine($"App will display information for banana.");
+    case 200:
+        title = "Senior Associate";
         break;
-
-    case "cherry":
-        Console.WriteLine($"App will display information for cherry.");
+    case 300:
+        title = "Manager";
+        break;
+    case 400:
+        title = "Senior Manager";
+        break;
+    default:
+        title = "Associate";
         break;
 }
+
+Console.WriteLine($"{employeeName}, {title}");
