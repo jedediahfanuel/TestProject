c8d298fd8e83fe7d5c0f5fa48a5e6dc963d2693e
jedediahfanuel <jedediahf51@gmail.com>
2024-07-14 17:47:45 +0700

    🩳 :: reassign variable in array inside loops

--- for-statement/Program.cs
+++ for-statement/Program.cs
@@ -19,8 +19,16 @@ for (int i = 0; i < 10; i++)
     if (i == 7) break;
 }
 
-string[] names = { "Alex", "Eddie", "David", "Michael" };
-for (int i = names.Length - 1; i >= 0; i--)
+string[] named = { "Alex", "Eddie", "David", "Michael" };
+for (int i = named.Length - 1; i >= 0; i--)
 {
-    Console.WriteLine(names[i]);
+    Console.WriteLine(named[i]);
 }
+
+Console.WriteLine("\n\n");
+
+string[] names = { "Alex", "Eddie", "David", "Michael" };
+for (int i = 0; i < names.Length; i++)
+    if (names[i] == "David") names[i] = "Sammy";
+
+foreach (var name in names) Console.WriteLine(name);
