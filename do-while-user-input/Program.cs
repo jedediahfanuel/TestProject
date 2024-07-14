26e9506eb16d448093c58c06ecb435719a3d9ac7
jedediahfanuel <jedediahf51@gmail.com>
2024-07-14 20:17:13 +0700

    🥾 :: project 1 valid numebr

--- do-while-user-input/Program.cs
+++ do-while-user-input/Program.cs
@@ -1,7 +1,6 @@
 ﻿string? readResult;
 bool validEntry = false;
 
-// capture user input in a string variable named readResult
 Console.WriteLine("Enter a string containing at least three characters:");
 do
 {
@@ -24,3 +23,36 @@ bool validNumber = false;
 
 validNumber = int.TryParse(readResult, out numericValue);
 
+// ----------------------------------------------------------
+// ----------------------------------------------------------
+// ----------------------------------------------------------
+// CODE PROJECT 1 - write code that validates integer input
+
+string? inputStr;
+bool isValid = false;
+int n = 0;
+
+Console.WriteLine("Enter an integer between 5 to 10");
+do
+{
+    inputStr = Console.ReadLine();
+    if (inputStr == null) continue;
+
+    isValid = int.TryParse(inputStr, out n);
+
+    if (!isValid)
+    {
+        Console.WriteLine("Sorry, you entered an invalid number, please try again");
+        continue;
+    }
+
+    if (n < 6 || n > 9)
+    {
+        isValid = false;
+        Console.WriteLine($"You entered {n}. Please enter a number between 5 and 10.");
+        continue;
+    }
+
+    Console.WriteLine($"Your input value ({inputStr}) has been accepted.");
+        
+} while (!isValid);
