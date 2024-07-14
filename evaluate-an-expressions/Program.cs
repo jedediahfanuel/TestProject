511d9e6c8ed6edf2a347b9344c0f646748dc2815
jedediahfanuel <jedediahf51@gmail.com>
2024-07-14 15:53:40 +0700

    🏫 :: ternary

--- evaluate-an-expressions/Program.cs
+++ evaluate-an-expressions/Program.cs
@@ -58,3 +58,14 @@ Console.WriteLine(a != b); // output: True
 string s1 = "Hello";
 string s2 = "Hello";
 Console.WriteLine(s1 != s2); // output: False
+
+Console.WriteLine("\n\n");
+
+// ------------------------------------------------------------------ Ternary
+
+Console.WriteLine("Ternary Operation");
+
+int saleAmount = 1001;
+// int discount = saleAmount > 1000 ? 100 : 50;
+
+Console.WriteLine($"Discount: {(saleAmount > 1000 ? 100 : 50)}");
