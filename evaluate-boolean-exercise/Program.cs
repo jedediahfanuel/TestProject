d8973b09086cacf8e032e43f62df953afea963f6
jedediahfanuel <jedediahf51@gmail.com>
2024-07-14 16:20:51 +0700

    🪩 :: exercise 2

--- evaluate-boolean-exercise/Program.cs
+++ evaluate-boolean-exercise/Program.cs
@@ -1,3 +1,17 @@
-﻿Random coin = new Random();
-int result = coin.Next(0,2);
-Console.WriteLine((result == 0) ? "heads" : "tails");
+﻿// First Exercise - FLIP COIN
+Random coin = new Random();
+int flip = coin.Next(0,2);
+Console.WriteLine((flip == 0) ? "heads" : "tails");
+
+// Second Exercise - DECISION LOGIC
+string permission = "Admin|Manager";
+int level = 55;
+
+string message = permission.Contains("Admin"  ) && level >  55 ? "Welcome, Super Admin user." 
+               : permission.Contains("Admin"  ) && level <= 55 ? "Welcome, Admin user."
+			   : permission.Contains("Manager") && level >= 20 ? "Contact an Admin for access."
+			   // : permission.Contains("Manager") && level <  20 ? 
+			   : "You don't have sufficient privileges.";
+
+Console.WriteLine(message);
+
