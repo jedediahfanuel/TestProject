071fb6c568670f1f8577d9d61cd4bd3baf9e891a
jedediahfanuel <jedediahf51@gmail.com>
2024-07-14 17:43:52 +0700

    👘 :: loop thru array

--- for-statement/Program.cs
+++ for-statement/Program.cs
@@ -18,3 +18,9 @@ for (int i = 0; i < 10; i++)
     Console.WriteLine(i);
     if (i == 7) break;
 }
+
+string[] names = { "Alex", "Eddie", "David", "Michael" };
+for (int i = names.Length - 1; i >= 0; i--)
+{
+    Console.WriteLine(names[i]);
+}
