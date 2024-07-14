d788f1f8e03a5a399ecb0bb1e4b7cac42814a2b9
jedediahfanuel <jedediahf51@gmail.com>
2024-07-14 18:01:33 +0700

    🕖️ :: fizzbuzz done

--- fizzbuzz/Program.cs
+++ fizzbuzz/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿for (int i = 1; i <= 100; i++)
+{
+	Console.Write(i);
+	if (i % 3 == 0 || i % 5 == 0) Console.Write(" - ");
+	if (i % 3 == 0              ) Console.Write("Fizz");
+	if (              i % 5 == 0) Console.Write("Buzz");
+	Console.WriteLine("");
+}
