9a3cbc5c922b3a3264d3c3e971e87fb82d95d67e
jedediahfanuel <jedediahf51@gmail.com>
2024-07-15 21:19:43 +0700

    👩‍ :: menu 2 loops

--- Guided-project-branching-looping-CSharp-main/GuidedProject/Starter/Program.cs
+++ Guided-project-branching-looping-CSharp-main/GuidedProject/Starter/Program.cs
@@ -130,8 +130,33 @@ do
             if (petCount < maxPets)
                 Console.WriteLine($"We currently have {petCount} pets that need homes. We can manage {(maxPets - petCount)} more.");
 
-            Console.WriteLine("Press the Enter key to continue.");
-            readResult = Console.ReadLine();
+            while (anotherPet == "y" && petCount < maxPets)
+            {
+                petCount++;
+                
+                if (petCount < maxPets)
+                {
+                    Console.WriteLine("Do you want to enter info for another pet (y/n)");
+
+                    do
+                    {
+                        readResult = Console.ReadLine();
+                        if (readResult != null)
+                        {
+                            anotherPet = readResult.ToLower();
+                        }
+
+                    } while (anotherPet != "y" && anotherPet != "n");
+                }
+            }
+
+            if (petCount >= maxPets)
+            {
+                Console.WriteLine("We have reached our limit on the number of pets that we can manage.");
+                Console.WriteLine("Press the Enter key to continue.");
+                readResult = Console.ReadLine();
+            }
+
             break;
         case "3":
             // Ensure animal ages and physical descriptions are complete
