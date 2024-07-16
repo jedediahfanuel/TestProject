6a2937de95821e59148ec3bd34a565781e03bb5f
jedediahfanuel <jedediahf51@gmail.com>
2024-07-16 23:05:34 +0700

    🍾 :: fix duplicate variable

--- casting-data/Program.cs
+++ casting-data/Program.cs
@@ -4,15 +4,28 @@ string result = first + second;
 Console.WriteLine(result);
 
 Console.WriteLine("Data Loss = Decimal to Int");
-decimal myDecimal = 3.14m;
-Console.WriteLine($"decimal: {myDecimal}");
+decimal myDecimala = 3.14m;
+Console.WriteLine($"decimal: {myDecimala}");
 
-int myInt = (int)myDecimal;
-Console.WriteLine($"int: {myInt}");
+int myInta = (int)myDecimala;
+Console.WriteLine($"int: {myInta}");
 
-Console.WriteLine("Narrowing Conversion"):
-decimal myDecimal = 1.23456789m;
-float myFloat = (float)myDecimal;
+Console.WriteLine("Narrowing Conversion");
+decimal smyDecimal = 1.23456789m;
+float smyFloat = (float)smyDecimal;
 
-Console.WriteLine($"Decimal: {myDecimal}");
-Console.WriteLine($"Float  : {myFloat}");
+Console.WriteLine($"Decimal: {smyDecimal}");
+Console.WriteLine($"Float  : {smyFloat}");
+
+
+Console.WriteLine("Performing Data Conversion\n");
+int ifirst = 5;
+int isecond = 7;
+string message = ifirst.ToString() + isecond.ToString();
+Console.WriteLine(message);
+
+string afirst = "5";
+string asecond = "7";
+int sum = int.Parse(afirst) + int.Parse(asecond);
+Console.WriteLine(sum);
+
