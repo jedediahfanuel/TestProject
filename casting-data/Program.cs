a21fb9a0b8ed705dd1566d6773bc7a09cc44c847
jedediahfanuel <jedediahf51@gmail.com>
2024-07-16 23:08:33 +0700

    🥚 :: rounding using Convert Class

--- casting-data/Program.cs
+++ casting-data/Program.cs
@@ -33,3 +33,9 @@ string value1 = "5";
 string value2 = "7";
 int hresult = Convert.ToInt32(value1) * Convert.ToInt32(value2);
 Console.WriteLine(hresult);
+
+int value = (int)1.5m; // casting truncates
+Console.WriteLine(value);
+
+int value2 = Convert.ToInt32(1.5m); // converting rounds up
+Console.WriteLine(value2);
