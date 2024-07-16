6ccdac42e3440df17ffb8b4dd88ca5ee7c885189
jedediahfanuel <jedediahf51@gmail.com>
2024-07-16 23:02:02 +0700

    🍯 :: narrowing conversion

--- casting-data/Program.cs
+++ casting-data/Program.cs
@@ -9,3 +9,10 @@ Console.WriteLine($"decimal: {myDecimal}");
 
 int myInt = (int)myDecimal;
 Console.WriteLine($"int: {myInt}");
+
+Console.WriteLine("Narrowing Conversion"):
+decimal myDecimal = 1.23456789m;
+float myFloat = (float)myDecimal;
+
+Console.WriteLine($"Decimal: {myDecimal}");
+Console.WriteLine($"Float  : {myFloat}");
