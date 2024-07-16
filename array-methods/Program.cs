2a2dad42ea50556080dd625b1dbe27666861ebe8
jedediahfanuel <jedediahf51@gmail.com>
2024-07-16 23:35:59 +0700

    🌽 :: as CSV

--- array-methods/Program.cs
+++ array-methods/Program.cs
@@ -53,5 +53,6 @@ Console.WriteLine("\n\n SPLIT && JOIN");
 string value = "abc123";
 char[] valueArray = value.ToCharArray();
 Array.Reverse(valueArray);
-string result = new string(valueArray);
+// string result = new string(valueArray);
+string result = String.Join(",", valueArray);
 Console.WriteLine(result);
