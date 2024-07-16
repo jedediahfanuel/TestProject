3456bbbdd34475dc4cccd0525bdd13c14d894195
jedediahfanuel <jedediahf51@gmail.com>
2024-07-16 23:26:31 +0700

    🥓 :: sort()

---
+++ array-methods/Program.cs
@@ -0,0 +1,9 @@
+﻿
+string[] pallets = { "B14", "A11", "B12", "A13" };
+
+Console.WriteLine("Sorted...");
+Array.Sort(pallets);
+foreach (var pallet in pallets)
+{
+    Console.WriteLine($"-- {pallet}");
+}
