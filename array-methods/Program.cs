da9f05e50ee9814a8293cf58d7d4914609ec545a
jedediahfanuel <jedediahf51@gmail.com>
2024-07-16 23:30:41 +0700

    🫑 :: reverse clear resize

--- array-methods/Program.cs
+++ array-methods/Program.cs
@@ -7,3 +7,34 @@ foreach (var pallet in pallets)
 {
     Console.WriteLine($"-- {pallet}");
 }
+
+Console.WriteLine("");
+Console.WriteLine("Reversed...");
+Array.Reverse(pallets);
+foreach (var pallet in pallets)
+{
+    Console.WriteLine($"-- {pallet}");
+}
+
+string[] paallets = { "B14", "A11", "B12", "A13" };
+Console.WriteLine("");
+
+Array.Clear(paallets, 0, 2);
+Console.WriteLine($"Clearing 2 ... count: {paallets.Length}");
+foreach (var paallet in paallets)
+{
+    Console.WriteLine($"-- {paallet}");
+}
+
+Console.WriteLine("");
+Array.Resize(ref pallets, 6);
+Console.WriteLine($"Resizing 6 ... count: {pallets.Length}");
+
+pallets[4] = "C01";
+pallets[5] = "C02";
+
+foreach (var pallet in pallets)
+{
+    Console.WriteLine($"-- {pallet}");
+}
+
