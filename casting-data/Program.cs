1ad1db78d69134605daab65f6f4ad5c2fc4dfd01
jedediahfanuel <jedediahf51@gmail.com>
2024-07-16 23:00:02 +0700

    🔪 :: data loss at conversion

--- casting-data/Program.cs
+++ casting-data/Program.cs
@@ -2,3 +2,10 @@
 string second = "4";
 string result = first + second;
 Console.WriteLine(result);
+
+Console.WriteLine("Data Loss = Decimal to Int");
+decimal myDecimal = 3.14m;
+Console.WriteLine($"decimal: {myDecimal}");
+
+int myInt = (int)myDecimal;
+Console.WriteLine($"int: {myInt}");
