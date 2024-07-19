f82a77704af3dc80f9db984f6760113fe4fdc9ee
jedediahfanuel <jedediahf51@gmail.com>
2024-07-19 20:04:30 +0700

    🍻 :: string interpolation

--- string-formatting-basics/Program.cs
+++ string-formatting-basics/Program.cs
@@ -1,4 +1,13 @@
-﻿string first = "Hello";
+﻿// String Format() Func
+string first = "Hello";
 string second = "World";
 string result = string.Format("{0} {1}!", first, second);
 Console.WriteLine(result);
+
+
+// String Interpolation
+string frst = "Hello";
+string scnd = "World";
+Console.WriteLine($"{frst} {scnd}!");
+Console.WriteLine($"{scnd} {frst}!");
+Console.WriteLine($"{frst} {frst} {frst}!");
