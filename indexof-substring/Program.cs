48b2977a85b5b0563d0665f6f4ab48d5154705a0
jedediahfanuel <jedediahf51@gmail.com>
2024-07-20 22:50:49 +0700

    🧇 :: indexof 1

---
+++ indexof-substring/Program.cs
@@ -0,0 +1,7 @@
+﻿string message = "Find what is (inside the parentheses)";
+
+int openingPosition = message.IndexOf('(');
+int closingPosition = message.IndexOf(')');
+
+Console.WriteLine(openingPosition);
+Console.WriteLine(closingPosition);
