df4b16763106a0ca8b741af8e626b39a02773ad7
jedediahfanuel <jedediahf51@gmail.com>
2024-07-20 23:35:42 +0700

    🥬 :: initial challenge

---
+++ challenge-extract-string/Program.cs
@@ -0,0 +1,9 @@
+﻿const string input = "<div><h2>Widgets &trade;</h2><span>5000</span></div>";
+
+string quantity = "";
+string output = "";
+
+// Your work here
+
+Console.WriteLine(quantity);
+Console.WriteLine(output);
