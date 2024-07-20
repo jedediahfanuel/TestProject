4045f7a237297892eae8e64ca67a11e38bf137cd
jedediahfanuel <jedediahf51@gmail.com>
2024-07-20 23:05:33 +0700

    👩 :: more than 1 parentheses

--- lastindexof-indexofany/Program.cs
+++ lastindexof-indexofany/Program.cs
@@ -1,8 +1,15 @@
-﻿
-string message = "(What if) I am (only interested) in the last (set of parentheses)?";
-int openingPosition = message.LastIndexOf('(');
+﻿string message = "(What if) there are (more than) one (set of parentheses)?";
+while (true)
+{
+    int openingPosition = message.IndexOf('(');
+    if (openingPosition == -1) break;
 
-openingPosition += 1;
-int closingPosition = message.LastIndexOf(')');
-int length = closingPosition - openingPosition;
-Console.WriteLine(message.Substring(openingPosition, length));
+    openingPosition += 1;
+    int closingPosition = message.IndexOf(')');
+    int length = closingPosition - openingPosition;
+    Console.WriteLine(message.Substring(openingPosition, length));
+
+    // Note the overload of the Substring to return only the remaining 
+    // unprocessed message:
+    message = message.Substring(closingPosition + 1);
+}
