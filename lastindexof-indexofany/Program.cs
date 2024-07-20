f3787b81f18c11d1ef886e373da3154d8e9832b7
jedediahfanuel <jedediahf51@gmail.com>
2024-07-20 23:08:20 +0700

    🍈 :: multi parentheses type

--- lastindexof-indexofany/Program.cs
+++ lastindexof-indexofany/Program.cs
@@ -13,3 +13,13 @@ while (true)
     // unprocessed message:
     message = message.Substring(closingPosition + 1);
 }
+
+string gmessageg = "Help (find) the {opening Sym}";
+Console.WriteLine($"Searching THIS gMessageg: {gmessageg}");
+char[] openSym = { '[', '{', '(' };
+int startPos = 5;
+int openingPos = gmessageg.IndexOfAny(openSym);
+Console.WriteLine($"Found WITHOUT using startPos: {gmessageg.Substring(openingPos)}");
+
+openingPos = gmessageg.IndexOfAny(openSym, startPos);
+Console.WriteLine($"Found WITH using startPos {startPos}:  {gmessageg.Substring(openingPos)}");
