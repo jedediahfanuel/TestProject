e20e5d3af5eeb5286a1c6377255f9c903ea06808
jedediahfanuel <jedediahf51@gmail.com>
2024-07-20 22:30:20 +0700

    🤲 :: example padding

--- exercise-string-interpolation/Program.cs
+++ exercise-string-interpolation/Program.cs
@@ -9,3 +9,20 @@ Console.WriteLine($"   Shares: {productShares:N3} Product");
 Console.WriteLine($"     Sub Total: {subtotal:C}");
 Console.WriteLine($"           Tax: {taxPercentage:P2}");
 Console.WriteLine($"     Total Billed: {total:C}");
+
+
+
+
+
+// Padding & Alignment
+string first = "Hello";
+string second = "World";
+string result = string.Format("{0} {1}!", first, second);
+Console.WriteLine(result);
+
+
+string input = "Pad this";
+Console.WriteLine(input.PadLeft(12));
+Console.WriteLine(input.PadRight(12));
+Console.WriteLine(input.PadLeft(12, '-'));
+Console.WriteLine(input.PadRight(12, '-'));
