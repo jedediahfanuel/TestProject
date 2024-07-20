eab97bf5514e0ef321883b63f26c476e53b61f3e
jedediahfanuel <jedediahf51@gmail.com>
2024-07-20 23:03:39 +0700

    💨 :: last index of 1

---
+++ lastindexof-indexofany/Program.cs
@@ -0,0 +1,8 @@
+﻿
+string message = "(What if) I am (only interested) in the last (set of parentheses)?";
+int openingPosition = message.LastIndexOf('(');
+
+openingPosition += 1;
+int closingPosition = message.LastIndexOf(')');
+int length = closingPosition - openingPosition;
+Console.WriteLine(message.Substring(openingPosition, length));
