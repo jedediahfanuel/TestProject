208ef13b0f54be9d902a824a46c9f7278e1055e5
jedediahfanuel <jedediahf51@gmail.com>
2024-07-21 14:35:59 +0700

    🍲 :: update methods

--- IPv4/Program.cs
+++ IPv4/Program.cs
@@ -9,9 +9,41 @@ then ipAddress is valid
 
 else ipAddress is invalid
 */
-void ValidateLength() {}
-void ValidateZeroes() {}
-void ValidateRange() {}
+string ipv4Input = "107.31.1.5";
+bool validLength = false;
+bool validZeroes = false;
+bool validRange = false;
+
+void ValidateLength()
+{
+	validLength = ipv4Input.Split(".").Length == 4
+}
+
+void ValidateZeroes()
+{
+	foreach (string number in ipv4Input)
+	{
+		if (number.Length > 1 && number.StartsWith("0"))
+		{
+			validZeroes = false;
+			return;
+		}
+	}
+	validZeroes = true;
+}
+
+void ValidateRange()
+{
+	foreach (string number in ipv4Input)
+	{
+		if (int.Parse(number) < 1 || int.Parse(number) > 255)
+		{
+			validRange = false;
+			return;
+		}
+	}
+	validRange = true;
+}
 
 if (ValidateLength() && ValidateZeroes() && ValidateRange()) 
 {
