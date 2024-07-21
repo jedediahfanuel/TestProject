a99e0312783309c9c84cfbc1061ef513cc6a9edb
jedediahfanuel <jedediahf51@gmail.com>
2024-07-21 21:48:49 +0700

    🏘️  :: dice game done

--- challenge-dice-game/Program.cs
+++ challenge-dice-game/Program.cs
@@ -12,14 +12,30 @@ void PlayGame()
 
     while (play) 
     {
-        var target;
-        var roll;
+        var target = GetRandom();
+        var roll = GetRandom();
 
-        Console.WriteLine($"Roll a number greater than {target} to win!");
+        Console.WriteLine($"\nRoll a number greater than {target} to win!");
         Console.WriteLine($"You rolled a {roll}");
-        Console.WriteLine(WinOrLose());
+        Console.WriteLine(WinOrLose(target, roll));
         Console.WriteLine("\nPlay again? (Y/N)");
 
         play = ShouldPlay();
     }
+}
+
+int GetRandom()
+{
+	return random.Next(0,11);
+}
+
+string WinOrLose(int target, int roll)
+{
+	return target < roll ? "You win!" : "You lose!";
+}
+
+bool ShouldPlay()
+{
+	string response = Console.ReadLine();
+	return response.ToLower().Equals("y");
 }
