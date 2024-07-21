313e3fc60b2d5fd3ce291046def4eb4e50b57569
jedediahfanuel <jedediahf51@gmail.com>
2024-07-21 20:48:04 +0700

    🎄 :: update function two coin

--- function/Program.cs
+++ function/Program.cs
@@ -114,20 +114,28 @@ bool IsPalindrome(string word)
 
 
 // ARRAY FUNCTION
-int[] TwoCoins(int[] coins, int target) 
+int[,] TwoCoins(int[] coins, int target) 
 {
+    int[,] result = {{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1}};
+    int count = 0;
+
     for (int curr = 0; curr < coins.Length; curr++) 
     {
         for (int next = curr + 1; next < coins.Length; next++) 
         {
             if (coins[curr] + coins[next] == target) 
             {
-                return new int[]{curr, next};
+                result[count, 0] = curr;
+                result[count, 1] = next;
+                count++;
             }
-
+            if (count == result.GetLength(0)) 
+            {
+                return result;
+            }
         }
     }
-    return  new int[0];
+    return (count == 0) ? new int[0,0] : result;
 }
 
 int target = 60;
