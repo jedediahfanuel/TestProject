aa23c9ea2b869a16bd93f54f9770a58abc5ea3ec
jedediahfanuel <jedediahf51@gmail.com>
2024-07-21 15:02:56 +0700

    🍭 :: example 3 radius

--- method-parameter/Program.cs
+++ method-parameter/Program.cs
@@ -36,3 +36,44 @@ void DisplayAdjustedTimes(int[] times, int currentGMT, int newGMT)
 }
 
 DisplayAdjustedTimes(schedule, 6, -6);
+
+
+// METHOD SCOPE
+string[] students = {"Jenna", "Ayesha", "Carlos", "Viktor"};
+
+DisplayStudents(students);
+DisplayStudents(new string[] {"Robert","Vanya"});
+
+void DisplayStudents(string[] students) 
+{
+    foreach (string student in students) 
+    {
+        Console.Write($"{student}, ");
+    }
+    Console.WriteLine();
+}
+
+
+// RADIUS
+double pi = 3.14159;
+PrintCircleInfo(12);
+PrintCircleInfo(24);
+
+void PrintCircleInfo(int radius) 
+{
+    Console.WriteLine($"Circle with radius {radius}");
+    PrintCircleArea(radius);
+    PrintCircleCircumference(radius);
+}
+
+void PrintCircleArea(int radius)
+{
+    double area = pi * (radius * radius);
+    Console.WriteLine($"Area = {area}");
+}
+
+void PrintCircleCircumference(int radius)
+{
+    double circumference = 2 * pi * radius;
+    Console.WriteLine($"Circumference = {circumference}");
+}
