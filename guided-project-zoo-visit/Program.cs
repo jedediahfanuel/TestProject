9178b525e8f0d8473f770d45a1dfe9b7186c9577
jedediahfanuel <jedediahf51@gmail.com>
2024-07-21 22:05:54 +0700

    ⛰️  :: main function

--- guided-project-zoo-visit/Program.cs
+++ guided-project-zoo-visit/Program.cs
@@ -7,10 +7,17 @@ string[] pettingZoo =
     "ostriches", "pigs", "ponies", "rabbits", "sheep", "tortoises",
 };
 
-RandomizeAnimals();
-string[,] group = AssignGroup();
-Console.WriteLine("School A");
-PrintGroup(group);
+PlanSchoolVisit("School A");
+PlanSchoolVisit("School B", 3);
+PlanSchoolVisit("School C", 2);
+
+void PlanSchoolVisit(string schoolName, int groups = 6) 
+{
+    RandomizeAnimals(); 
+    string[,] group1 = AssignGroup(groups);
+    Console.WriteLine(schoolName);
+    PrintGroup(group1);
+}
 
 void RandomizeAnimals() 
 {
@@ -42,14 +49,14 @@ string[,] AssignGroup(int groups = 6)
     return result;
 }
 
-void PrintGroup(string[,] group) 
+void PrintGroup(string[,] groups) 
 {
-    for (int i = 0; i < group.GetLength(0); i++) 
+    for (int i = 0; i < groups.GetLength(0); i++) 
     {
         Console.Write($"Group {i + 1}: ");
-        for (int j = 0; j < group.GetLength(1); j++) 
+        for (int j = 0; j < groups.GetLength(1); j++) 
         {
-            Console.Write($"{group[i,j]}  ");
+            Console.Write($"{groups[i,j]}  ");
         }
         Console.WriteLine();
     }
