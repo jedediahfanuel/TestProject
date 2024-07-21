b20543131ffdb9e49c993b1774d20f84246b8d04
jedediahfanuel <jedediahf51@gmail.com>
2024-07-21 14:24:40 +0700

    🧨 :: initial exercise

---
+++ IPv4/Program.cs
@@ -0,0 +1,11 @@
+﻿/*
+if ipAddress consists of 4 numbers
+and
+if each ipAddress number has no leading zeroes
+and
+if each ipAddress number is in range 0 - 255
+
+then ipAddress is valid
+
+else ipAddress is invalid
+*/
