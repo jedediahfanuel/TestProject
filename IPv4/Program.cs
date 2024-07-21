913f4add87100e6c87535fd29407323e509a6ceb
jedediahfanuel <jedediahf51@gmail.com>
2024-07-21 14:25:59 +0700

    🫔 :: wireframe

--- IPv4/Program.cs
+++ IPv4/Program.cs
@@ -9,3 +9,15 @@ then ipAddress is valid
 
 else ipAddress is invalid
 */
+void ValidateLength() {}
+void ValidateZeroes() {}
+void ValidateRange() {}
+
+if (ValidateLength() && ValidateZeroes() && ValidateRange()) 
+{
+    Console.WriteLine($"ip is a valid IPv4 address");
+} 
+else 
+{
+    Console.WriteLine($"ip is an invalid IPv4 address");
+}
