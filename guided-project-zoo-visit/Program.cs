95d975f7b0d9d6f7e29ab71a923b9022bf625e0b
jedediahfanuel <jedediahf51@gmail.com>
2024-07-21 21:59:56 +0700

    🎡 :: randomize array

--- guided-project-zoo-visit/Program.cs
+++ guided-project-zoo-visit/Program.cs
@@ -6,3 +6,26 @@ string[] pettingZoo =
     "goats", "iguanas", "kangaroos", "lemurs", "llamas", "macaws", 
     "ostriches", "pigs", "ponies", "rabbits", "sheep", "tortoises",
 };
+
+RandomizeAnimals();
+// string[,] group = AssignGroup();
+Console.WriteLine("School A");
+// PrintGroup(group);
+
+void RandomizeAnimals() 
+{
+    Random random = new Random();
+
+    for (int i = 0; i < pettingZoo.Length; i++) 
+    {
+        int r = random.Next(i, pettingZoo.Length);
+
+        string temp = pettingZoo[r];
+        pettingZoo[r] = pettingZoo[i];
+        pettingZoo[i] = temp;
+    }
+}
+foreach(string animal in pettingZoo) 
+{
+    Console.WriteLine(animal);
+}
