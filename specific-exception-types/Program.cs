5167bf4b1ce90b6d42bf0387937cee7daef4a175
jedediahfanuel <jedediahf51@gmail.com>
2024-07-22 22:30:46 +0700

    🦃  :: multiple exceptions

--- specific-exception-types/Program.cs
+++ specific-exception-types/Program.cs
@@ -27,7 +27,26 @@ static void WriteMessage()
     double float2 = 0.0;
     int number1 = 3000;
     int number2 = 0;
+    byte smallNumber;
 
-    Console.WriteLine(float1 / float2);
-    Console.WriteLine(number1 / number2);
+    try
+    {
+        Console.WriteLine(float1 / float2);
+        Console.WriteLine(number1 / number2);
+    }
+    catch (DivideByZeroException ex)
+    {
+        Console.WriteLine($"Exception caught in WriteMessage: {ex.Message}");
+    }
+    checked
+    {
+        try
+        {
+            smallNumber = (byte)number1;
+        }
+        catch (OverflowException ex)
+        {
+            Console.WriteLine($"Exception caught in WriteMessage: {ex.Message}");
+        }  
+    }
 }
