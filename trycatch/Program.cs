31e89bd37b2443b3ec3014d1e3e3e8b7bb875939
jedediahfanuel <jedediahf51@gmail.com>
2024-07-22 22:15:45 +0700

    🏗️ :: tc

--- trycatch/Program.cs
+++ trycatch/Program.cs
@@ -3,6 +3,14 @@ double float2 = 0.0;
 int number1 = 3000;
 int number2 = 0;
 
-Console.WriteLine(float1 / float2);
-Console.WriteLine(number1 / number2);
+try
+{
+    Console.WriteLine(float1 / float2);
+    Console.WriteLine(number1 / number2);
+}
+catch (System.Exception)
+{
+    Console.WriteLine("An exception has been caught");
+}
+
 Console.WriteLine("Exit program");
